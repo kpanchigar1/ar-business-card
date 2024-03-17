f06a5f4128ed9f345f46ab1c07d547ae42d527fc
Jess Berry <jberry3@sheffield.ac.uk>
2024-03-17 14:54:38 +0000

    final overview.wav file, 9 seconds!!!!!

--- js/overview.js
+++ js/overview.js
@@ -1,4 +1,4 @@
 const textToSpeechGenerator = require('./textToSpeech.js');
 
-textToSpeechGenerator('I am John McNamara, master inventor for IBM!', 'overview.wav');
+textToSpeechGenerator('I’m John McNamara, an IBM Master Inventor, Honorary Professor, Research Fellow, Impact Fellow and I currently lead IBM UK University Programs.', 'overview.wav');
 
