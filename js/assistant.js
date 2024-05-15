9e948c215c02918170fd502179ff3bbefc2cfa12
NawarUoS <NAbouTrabi1@sheffield.ac.uk>
2024-05-15 15:17:46 +0100

    Added documentation to assisstant.js

--- js/assistant.js
+++ js/assistant.js
@@ -1,41 +1,44 @@
+// Import necessary modules from ibm-watson package
 let AssistantV2 = require('ibm-watson/assistant/v2');
 let { BearerTokenAuthenticator } = require('ibm-watson/auth');
 
-
+// Fetch an access token from a Netlify serverless function
 let assistantPromise = fetch('/.netlify/functions/get-token')
     .then(response => response.json())
     .then(data => {
+        // Extract the access token from the response
         const accessToken = data.access_token;
         console.log("Access Token fetched");
 
-        // Use the access_token to authenticate requests
+        // Create an instance of AssistantV2 with BearerTokenAuthenticator for authentication
         let assistant = new AssistantV2({
-            version: '2021-06-14',
-            url: "https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/cd153831-2882-424a-921f-cd367fc10c99",
+            version: '2021-06-14', // Specify the version of Watson Assistant API
+            url: "https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/cd153831-2882-424a-921f-cd367fc10c99", // Specify the URL of your Watson Assistant instance
             authenticator: new BearerTokenAuthenticator({
-                bearerToken: accessToken,
+                bearerToken: accessToken, // Provide the access token for authentication
             }),
-            //serviceUrl: "https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/cd153831-2882-424a-921f-cd367fc10c99", // use environment variable
         });
-        //assistant.setServiceUrl(process.env.SERVICE_URL);
         console.log("Assistant created");
-        return assistant;
+        return assistant; // Returns the assistant instance
     })
     .catch(error => console.error('Error:', error));
 
-
+// Function to interact with the chatbot
 function chatbot(question){
-    console.log("chatbot function called")
+    console.log("chatbot function called");
     return assistantPromise.then(assistant => {
-        console.log("assistant:" + assistant)
+        console.log("assistant:" + assistant);
+        // Send a stateless message to the assistant with the user's question
         return assistant.messageStateless({
-            assistantId: 'dd0e8243-5e9b-474f-9e67-b07a0eec17df', // use environment variable
+            assistantId: 'dd0e8243-5e9b-474f-9e67-b07a0eec17df', // Specifies
+            // the ID of the Watson Assistant instance
             input: {
                 'message_type': 'text',
                 'text': question
             }
         })
             .then(res => {
+                // Extract and return the response text from the Watson Assistant response
                 let responseText = res.result.output.generic[0].text;
                 return responseText;
             })
@@ -44,4 +47,6 @@ function chatbot(question){
             });
     });
 };
+
+// Export the chatbot function for use in other modules
 module.exports = chatbot;
